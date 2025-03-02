39525b7eea5ebe064a3c2589566f47acc4beb235
jaypso <joanapaulasoliveira@gmail.com>
2025-03-01 22:23:26 -0300

    pmb deploy vite correction 2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { viteStaticCopy } from 'vite-plugin-static-copy'; // Use named import
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
         viteStaticCopy({
             targets: [
-                { src: 'resources/images/*', dest: 'images' }, // Copies all images to public/images
+                { src: 'resources/images/logo.png', dest: 'images' }, // Specific file
             ],
         }),
         laravel({
