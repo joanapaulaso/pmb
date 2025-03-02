1867c05e42d9cc617d335b5def88bc07f6ac2e3e
jaypso <joanapaulasoliveira@gmail.com>
2025-03-01 22:31:34 -0300

    pmb deploy vite correction 4

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import logo from '../images/logo.png';
 
 window.Alpine = Alpine;
 Livewire.start();
--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,18 @@ export default defineConfig({
     plugins: [
         viteStaticCopy({
             targets: [
-                { src: 'resources/images/logo.png', dest: 'images' }, // Specific file
+                { src: 'resources/images/logo.png', dest: 'images' }, // Ensure logo.png exists
             ],
         }),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'], // Use array for multiple entry points
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
     build: {
         outDir: 'public/build',
         assetsDir: 'assets',
-        // Remove or align rollupOptions.input to match laravel.input
         rollupOptions: {
-            // Optionally, specify input here if needed, but it should match laravel.input
             input: {
                 app: 'resources/js/app.js',
                 appCss: 'resources/css/app.css', // Add CSS as a named entry
